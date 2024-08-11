118515ad0393aa6c4d12f93c682c3ad1ddfa5bcd
brunoeduardoed433 <brunoeduardoed433@gmail.com>
2024-08-11 19:41:13 -0300

    Adicionando_Assets_Temporarios

--- Dengue_Action.py
+++ Dengue_Action.py
@@ -4,15 +4,22 @@ from sys import exit
 
 pygame.init()
 
-largura = 640
-altura = 480
+largura = 1280
+altura = 720
 
 tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('Dengue Action')
+titulo  = pygame.display.set_caption('Dengue Action')
+
+bg = pygame.image.load("assets/BG.jpg")
+player = pygame.image.load("assets/bola.png")
+
+def desenhar():
+    tela.blit(bg, (0,0))
+    tela.blit(player, (0,670))
 
 while True:
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == pygame.QUIT:
             pygame.quit()
             exit()
     pygame.display.update()
